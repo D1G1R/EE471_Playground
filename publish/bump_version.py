83c4252a1eb1d44fa37fd78ad806d29b7a10a346
Ugurcan <265626936+D1G1R@users.noreply.github.com>
2026-06-04 16:07:15 +0300

    fix: auto-format python files with pre-commit

--- publish/bump_version.py
+++ publish/bump_version.py
@@ -4,8 +4,8 @@ import os
 if len(sys.argv) > 1:
     new_version = sys.argv[1]
     # Kök dizindeki VERSION dosyasının yolunu belirliyoruz
-    version_file_path = os.path.join(os.path.dirname(__file__), '..', 'VERSION')
-    
+    version_file_path = os.path.join(os.path.dirname(__file__), "..", "VERSION")
+
     with open(version_file_path, "w") as f:
         f.write(new_version)
     print(f"Versiyon güncellendi: {new_version}")
